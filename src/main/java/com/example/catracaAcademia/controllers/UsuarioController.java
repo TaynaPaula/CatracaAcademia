4ea69c48d5cb5668c63fe5ac06fa339e4e9fd1a9
TaynaPaula <taynapaulacruz@gmail.com>
2025-03-19 16:17:46 -0300

    Add de NotNull, Not blank e listas de usuarios

--- src/main/java/com/example/catracaAcademia/controllers/UsuarioController.java
+++ src/main/java/com/example/catracaAcademia/controllers/UsuarioController.java
@@ -1,25 +1,38 @@
 package com.example.catracaAcademia.controllers;
 
+import com.example.catracaAcademia.model.Usuario;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/usuario") // Define o prefixo base para todas as rotas deste controlador
 public class UsuarioController {
-
     
+    private List<Usuario> usuarios = new ArrayList<>();
+
     @GetMapping("/findbyname")
     public String getUsuario(@RequestParam(value = "name") String name) {
-        return "Olá, usuário: " + name + "Seja bem-vindo!";
+        return "Olá, usuário: " + name + " Seja bem-vindo!";
     }
 
-   
     @GetMapping("/get/{id}")
     public String getUsuarioById(@PathVariable String id) {
         return "Usuário com ID: " + id;
+    }
+
+    @PostMapping("/usuarios") // <-- Aqui garantimos que "/usuarios" existe
+    public ResponseEntity<Usuario> inserirUsuario(@RequestBody @Valid Usuario usuario) {
+        usuarios.add(usuario);
+        System.out.println("Usuário inserido com sucesso: " + usuario);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Usuario>> listarUsuarios() {
+        return ResponseEntity.ok(usuarios);
     }
 }
